6037d3795d70110cd2c49203a2c12716a9dc1513
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-04-10 20:12:47 +0800

    Initial Commit

---
+++ HockeyArenaCalculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HockeyArenaCalculator
+//
+//  Created by Luis Fernando Mata on 4/10/19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HockeyArenaCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HockeyArenaCalculator
+//
+//  Created by Luis Fernando Mata on 4/10/19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
